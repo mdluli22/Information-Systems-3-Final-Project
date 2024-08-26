23f62db5803bdca0309adc051763cfd1fb9ded92
Akhona Mdluli <G22M1142@ict.ru.ac.za>
2024-08-26 14:05:51 +0200

    updated

--- ticket_creation/ticketCreation.php
+++ ticket_creation/ticketCreation.php
@@ -19,8 +19,15 @@
         // include database details from config.php file
         require_once("config.php");
 
+        //for the res name on top
         $resName = $_REQUEST['residence'];
         $studentID =$_REQUEST['username'];
+
+        //for fault category
+        if (isset($_REQUEST['submit'])) {
+            $fault = $_REQUEST['fault-category'];
+        
+
         // attempt to make database connection
         $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DATABASE);
 
@@ -31,7 +38,8 @@
 
         // query instructions
         // Prepare and execute the query
-        $sql1 = "SELECT resName FROM student WHERE studentID = ?";
+        //for the res name on top
+        $sql1 = "SELECT resName FROM student WHERE studentID = $studentID ";
         $stmt = $conn->prepare($sql1);
         $stmt->bind_param("i", $studentID); // Bind the student ID as an integer
         $stmt->execute();
@@ -41,9 +49,23 @@
         if ($result === FALSE) {
             die("<p class=\"error\">Query was Unsuccessful!</p>");
         }
+
+        $sql2 = "INSERT INTO fault_categories (category) VALUES (?)";
+        $stmt1 = $conn -> prepare($sql2);
+        $stml -> bind_parm("s", $fault);
+
+        //design an error pop up
+        if ($stmt->execute()) {
+            echo "<p class=\"success\">Fault category successfully inserted into the database!</p>";
+        } else {
+            echo "<p class=\"error\">Failed to insert fault category!</p>";
+        }
+        
         
         // close connection
+        $stmt->close();
         $conn->close();
+    }
     ?>
 </body>
 </html>

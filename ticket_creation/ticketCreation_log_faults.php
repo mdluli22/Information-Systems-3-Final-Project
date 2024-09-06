77f1b70edb550a94a57a1a84bbd1ffda451bf9bc
Thokozile Tshabalala <G21T1001@ict.ru.ac.za>
2024-09-06 15:18:00 +0200

    modified the landing page

--- ticket_creation/ticketCreation_log_faults.php
+++ ticket_creation/ticketCreation_log_faults.php
@@ -103,17 +103,10 @@
             
             <nav>
                 <ul>
-<<<<<<< HEAD:ticket_creation/ticketCreation.php
-                    <li id="logFaults"><a href="../ticket_creation/ticketCreation.php"><img src="pictures/receipt-add.png" alt="receipt-add">Log faults</a></li> <!--style="background-color: #A020F0;" -->
-                    <li id="allTickets"><a href="../ticket_tracking/ticket_tracking_all.php"><img src="pictures/receipt-icon.png" alt="receipt-icon">All Tickets</a></li>
-                    <li id="openTickets"><a href="../ticket_tracking/ticket_tracking_open.php"><img src="pictures/layer.png" alt="layer">Open Tickets</a></li>
-                    <li id="closedTickets"><a href="../ticket_tracking/ticket_tracking_closed.php"><img src="pictures/clipboard-tick.png" alt="clipboard-tick">Closed Tickets</a></li>
-=======
                     <li id="logFaults"><a class="sidebar-links active" href="ticketCreation_log_faults.php"><img src="pictures/receipt-add.png" alt="receipt-add">Log Faults</a></li> <!--style="background-color: #A020F0;" -->
-                    <li id="allTickets"><a class="sidebar-links" href="ticketCreation_all_tickets.php"><img src="pictures/receipt-icon.png" alt="receipt-icon">All Tickets</a></li>
+                    <li id="allTickets"><a class="sidebar-links" href="ticket_tracking.php"><img src="pictures/receipt-icon.png" alt="receipt-icon">All Tickets</a></li>
                     <li id="openTickets"><a class="sidebar-links" href="ticketCreation_opened_tickets.php"><img src="pictures/layer.png" alt="layer">Opened Tickets</a></li>
                     <li id="closedTickets"><a class="sidebar-links" href="ticketCreation_closed_tickets.php"><img src="pictures/clipboard-tick.png" alt="clipboard-tick">Closed Tickets</a></li>
->>>>>>> 89ffbde40d67f2672f33b90fd9d961a44de28d7d:ticket_creation/ticketCreation_log_faults.php
                     <!-- <li id="statistics"><a class="sidebar-links" href="#"><img src="images/bar-chart-icon.png" alt="bar chart icon">Statistics</a></li>height="18px" -->
                 </ul>
             </nav>

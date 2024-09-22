ab6d98403a09a79e2d1345532d251bd6a4cab941
Amogelang Mphela <G22A7399@ict.ru.ac.za>
2024-09-22 15:24:30 +0200

    hall sec dashnoard

--- maintenance_dashboard/secure.php
+++ maintenance_dashboard/secure.php
@@ -8,7 +8,7 @@
 <body>
     <?php
     session_start();
-    if (!isset($_SESSION['access'])) {
+    if (!isset($_SESSION['access'])||!($_SESSION['user_role'] == "maintenance_staff")) {
         header("Location: ../landing_page/home.php");
     }
     ?>
